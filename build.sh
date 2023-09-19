5e79a546a2412aa5e4ff49fd335b4f5c45cc3a7b
MTRNord <mtrnord1@gmail.com>
2023-09-19 13:51:44 +0200

    poof

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-flatpak-builder --user --repo=steam --force-clean build-dir com.valvesoftware.Steam.yml
+flatpak-builder --user --repo=steam --force-clean --install-deps-from=flathub build-dir com.valvesoftware.Steam.yml
 flatpak --user remote-add --no-gpg-verify --if-not-exists steam steam
 flatpak install steam com.valvesoftware.Steam
 flatpak update com.valvesoftware.Steam
